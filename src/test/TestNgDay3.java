24f9b9c2a43595686228d97629332a76ac106240
Subs <subratpattanaik20@gmail.com>
2022-05-06 15:30:22 +0530

    TestNg XML approach

---
+++ src/test/TestNgDay3.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestNgDay3 {
+
+	@Test
+	public void webLoginCarLoan() {
+		//write selenium code how to log in
+		System.out.println("Web login car");
+	}
+	@Test
+	public void mobileLoginCarLoan() {
+		//write appium code, how to log in through phone/mobile
+		System.out.println("Mobile login car");
+	}
+	@Test
+	public void apiLoginCarLoan() {
+		//write rest api code, how to log in through api
+		System.out.println("API login car");
+	}
+}
---
+++ src/test/TestNgDay4.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestNgDay4 {
+
+	@Test
+	public void webLoginHomeLoan() {
+		//write selenium code how to log in
+		System.out.println("Web login home");
+	}
+	@Test
+	public void mobileLoginHomeLoan() {
+		//write appium code, how to log in through phone/mobile
+		System.out.println("Mobile login home");
+	}
+	@Test
+	public void apiLoginHomeLoan() {
+		//write rest api code, how to log in through api
+		System.out.println("API login home");
+	}
+}
