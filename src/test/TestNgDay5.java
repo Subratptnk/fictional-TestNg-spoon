8da04aeff26d4974fb863506aaf65538b078dcf4
Subs <subratpattanaik20@gmail.com>
2022-05-06 22:58:21 +0530

    Updated with Data providers and parameterization

--- src/test/TestNgDay5.java
+++ src/test/TestNgDay5.java
@@ -4,6 +4,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNgDay5 {
@@ -37,6 +39,49 @@ public class TestNgDay5 {
 	public void testcase7() {
 		System.out.println("This is test cases- 7");
 	}
+	@Parameters({"URL"})
+	@Test(enabled = false)
+	public void testcase8(String urlName) {
+		System.out.println("This is test cases- 8");
+		System.out.println("The Parameter name is "+urlName);
+	}
+	@Test(dataProvider = "getData")
+	public void testcase9(String username,String password) {
+		System.out.println("My Data provider");
+		System.out.println("My username is "+username);
+		System.out.println("My password is "+password);
+		
+	}
+	
+	
+	@DataProvider
+	@Test
+	public Object[][] getData() {
+		//System.out.println("This is test cases- 9");
+		
+		//1st - combination -- using one set of username and password.
+		//2nd - Combination -- using 2nd set of username and password.
+		// 3rd - Combination
+		
+		Object[][]data = new Object[3][2]; // Object[number of combinations][on each combination how many values are passing]
+		// couloumns in the row are nothing but values for that particualar combination(row)
+		//set=1
+		data[0][0] = "FirstSetUserName";
+		data[0][1] = "password1";
+		
+		//set = 2
+		data[1][0] = "SecondSetUserName";
+		data[1][1] = "password2";
+		
+		//set=3
+		data[2][0] = "ThirdSetUserName";
+		data[2][1] = "password3";
+		
+		return data;
+		
+		
+	}
+	
 	
 	
 	
